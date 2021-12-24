a5a83088ff74d884d0380b989139be35d46a3e9c
Erik Mulder <ewjmulder@yahoo.com>
2021-12-24 10:11:11 +0100

    dec24 done, was not that hard after all :)

--- src/2021/dec24/puzzle1.py
+++ src/2021/dec24/puzzle1.py
@@ -1,4 +1,49 @@
+from typing import Dict
+
 from src.util import *
 
-x = parse_
+instruction_lines = [line.split() for line in Parser.from_file(INPUT).to_lines()]
 
+
+def calculate(digit_nr: int, prev_output: Dict[int, int]) -> Dict[int, int]:
+    print(f"Calculating for digit: {digit_nr} with prev_output size: {len(prev_output)}")
+    instructions = instruction_lines[(digit_nr - 1) * 18 + 1:digit_nr * 18]
+    key_addition = instructions[4][2]
+    should_split = key_addition[0] == "-"
+    next_output = {}
+    for prev_answer, prev_number in prev_output.items():
+        for next_digit in range(1, 10):
+            register = {"w": next_digit, "x": 0, "y": 0, "z": prev_answer}
+            for instruction in instructions:
+                action = instruction[0]
+                a = instruction[1]
+                b = instruction[2]
+                if "wxyz".__contains__(b):
+                    b = register[b]
+                else:
+                    b = int(b)
+                if action == "add":
+                    register[a] = register[a] + b
+                elif action == "mul":
+                    register[a] = register[a] * b
+                elif action == "div":
+                    register[a] = int(register[a] / b)
+                elif action == "mod":
+                    register[a] = register[a] % b
+                elif action == "eql":
+                    register[a] = 1 if register[a] == b else 0
+            next_answer = register["z"]
+            next_number = prev_number * 10 + next_digit
+            next_output[next_answer] = next_number
+
+    if digit_nr == 14:
+        return next_output
+    elif should_split:
+        min_val = min(next_output.keys())
+        next_output = {answer: number for answer, number in next_output.items() if answer < min_val * 10}
+    return calculate(digit_nr + 1, next_output)
+
+
+answers = calculate(1, {0: 0})
+print(answers)
+print(len(answers))
